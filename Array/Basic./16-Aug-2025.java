e6d69da7907a82bc0ea5c47134e5ab138b67e437
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-08-16 22:36:58 +0530

    Create 16-Aug-2025.java

---
+++ Array/Basic./16-Aug-2025.java
@@ -0,0 +1,31 @@
+// Alternates in an Array -> 006
+
+class Solution {
+    // function to print alternate elements of an array
+    public ArrayList<Integer> getAlternates(int arr[]) {
+        // Code Here
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < arr.length; i += 2){
+            list.add(arr[i]);
+        }
+        
+        return list;
+    }
+}
+
+// Alternates in an Array
+
+class Solution {
+    // function to print alternate elements of an array
+    public ArrayList<Integer> getAlternates(int arr[]) {
+        // Code Here
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < arr.length; i += 2){
+            list.add(arr[i]);
+        }
+        
+        return list;
+    }
+}
