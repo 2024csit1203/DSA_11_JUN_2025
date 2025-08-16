62bbf2098e1f82738c23992c19b27d2be0a42ecc
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-08-16 22:43:08 +0530

    Update 16-Aug-2025.java

--- Array/Basic./16-Aug-2025.java
+++ Array/Basic./16-Aug-2025.java
@@ -14,18 +14,3 @@ class Solution {
     }
 }
 
-// Alternates in an Array
-
-class Solution {
-    // function to print alternate elements of an array
-    public ArrayList<Integer> getAlternates(int arr[]) {
-        // Code Here
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        for(int i = 0; i < arr.length; i += 2){
-            list.add(arr[i]);
-        }
-        
-        return list;
-    }
-}
