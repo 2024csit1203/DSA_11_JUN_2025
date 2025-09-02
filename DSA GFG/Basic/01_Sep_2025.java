044128d97a7b6f0b03955dbfe51d6b3c74c7b6fd
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-09-02 21:03:47 +0530

    Update 01_Sep_2025.java

--- DSA GFG/Basic/01_Sep_2025.java
+++ DSA GFG/Basic/01_Sep_2025.java
@@ -1,1 +1,40 @@
+//  Factorial
 
+class Solution {
+    // Function to calculate factorial of a number.
+    int factorial(int n) {
+        // code here
+        int fact = 1;
+        
+        for(int i = 1; i <= n; i++){
+            fact *= i;
+        }
+        
+        return fact;
+    }
+}
+
+//  Sum of Natural Numbers
+
+class Solution {
+    public static int findSum(int n) {
+        // code here
+        
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+        
+        return sum;
+    }
+}
+
+// Sum of first n terms
+
+class Solution {
+    int sumOfSeries(int n) {
+        // code here
+        int sum  = n * (n + 1) / 2;
+        return sum * sum;
+    }
+}
