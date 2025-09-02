e63e3fe59b861153e4cd3a8ca72cda1be829c54e
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-09-02 23:11:27 +0530

    Update 01_Sep_2025.java

--- DSA GFG/Basic/01_Sep_2025.java
+++ DSA GFG/Basic/01_Sep_2025.java
@@ -1,4 +1,4 @@
-//  Factorial
+//  Factorial -> 01 Sep 2025
 
 class Solution {
     // Function to calculate factorial of a number.
@@ -14,7 +14,7 @@ class Solution {
     }
 }
 
-//  Sum of Natural Numbers
+//  Sum of Natural Numbers -> 01 Sep 2025
 
 class Solution {
     public static int findSum(int n) {
@@ -29,12 +29,60 @@ class Solution {
     }
 }
 
-// Sum of first n terms
+// Sum of first n terms -> 01 Sep 2025
 
 class Solution {
     int sumOfSeries(int n) {
         // code here
         int sum  = n * (n + 1) / 2;
         return sum * sum;
+    }
+}
+
+// Odd or Even  -> 02 Sep 2025
+
+class Solution {
+    static boolean isEven(int n) {
+        // code here
+        if(n % 2 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+ 
+// Count Squares  -> 02 Sep 2025
+
+class Solution {
+    static int countSquares(int n) {
+        // code here
+        
+        int count = 0;
+        
+        int num = 1;
+        
+        int squre = 1;
+        
+        while(squre < n){
+            count++;
+            num++;
+            squre = num * num;
+        }
+        
+        return count;
+    }
+}
+
+// Addition of Two Numbers  -> 02 Sep 2025
+
+class Solution {
+    static int addition(int a, int b) {
+        // code here
+        
+        int ans = a + b;
+        
+        return ans;
     }
 }
