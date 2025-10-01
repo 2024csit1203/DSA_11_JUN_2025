901dfacee4e0bc86cae2e5ce7b7d6b9cb5eccaea
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-10-01 21:43:23 +0530

    Create Oct_2025.java

---
+++ DSA GFG/Basic/Oct_2025.java
@@ -0,0 +1,14 @@
+// Sum of Array -> 01 Oct 2025
+class Solution {
+    int arraySum(int arr[]) {
+        // code here
+        int sum = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        
+        return sum;
+        
+    }
+}
