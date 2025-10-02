bfd261e9643e4d440bc64ee0c53de5a522ab4f3d
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-10-02 22:51:11 +0530

    Update Oct_2025.java

--- DSA GFG/Basic/Oct_2025.java
+++ DSA GFG/Basic/Oct_2025.java
@@ -12,3 +12,20 @@ class Solution {
         
     }
 }
+
+// Count of smaller elements -> 02 Oct 2025
+
+class Solution {
+    public int countOfElements(int x, List<Integer> arr) {
+        // Code here
+        int count = 0;
+        
+        for(int i = 0; i < arr.size(); i++){
+            if(arr.get(i) <= x){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
