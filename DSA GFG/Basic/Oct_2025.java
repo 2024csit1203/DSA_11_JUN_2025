676b1cfe728f4cee8b077f9a0ed0e62537c3ecea
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-10-03 21:59:21 +0530

    Update Oct_2025.java

--- DSA GFG/Basic/Oct_2025.java
+++ DSA GFG/Basic/Oct_2025.java
@@ -29,3 +29,24 @@ class Solution {
         return count;
     }
 }
+
+// Count Odd and Even -> 03 Oct 2025
+
+class Solution {
+    public int[] countOddEven(int[] arr) {
+        // Code here
+        int odd = 0;
+    int even = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    int ans[] = new int[] {odd, even};
+    return ans;
+    }
+}
