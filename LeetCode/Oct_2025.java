d5f07aeb1fd5cd4c543d1ac121a88d2657cb7775
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-10-12 14:42:22 +0530

    Update Oct_2025.java

--- LeetCode/Oct_2025.java
+++ LeetCode/Oct_2025.java
@@ -16,3 +16,24 @@ class Solution {
         return ans;
     }
 }
+// 35. Search Insert Position -> 12 Oct 2025
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int ans = -1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] >= target){
+                ans = i;
+                break;
+            }
+
+        }
+        
+        if(ans == -1){
+            ans = nums.length;
+        }
+
+        return ans;
+
+    }
+}
