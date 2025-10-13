33e5e17903453b33d9e0dbad6ef8c6f6393ce029
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-10-13 19:39:12 +0530

    Update Oct_2025.java

--- LeetCode/Oct_2025.java
+++ LeetCode/Oct_2025.java
@@ -37,3 +37,23 @@ class Solution {
 
     }
 }
+
+// 066. Plus One -> 13 Oct 2025
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;  // set to 0 and carry 1
+        }
+
+        // If all digits were 9 (e.g. 999 → 1000)
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+}
