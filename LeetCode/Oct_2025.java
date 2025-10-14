03d7b27533e716dd335d53579c95cd5cc2d8a9f9
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-10-14 23:52:16 +0530

    Update Oct_2025.java

--- LeetCode/Oct_2025.java
+++ LeetCode/Oct_2025.java
@@ -57,3 +57,18 @@ public class Solution {
         return newDigits;
     }
 }
+
+// 136. Single Number -> 14 Oct 2025
+
+class Solution {
+    public int singleNumber(int[] nums) {
+       Arrays.sort(nums);
+
+    for (int i = 0; i < nums.length - 1; i += 2) {
+        if (nums[i] != nums[i + 1]) {
+            return nums[i];
+        }
+    }
+    return nums[nums.length - 1]; 
+    }
+}
