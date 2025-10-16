a972ca709b80f1837ce74eadb5bc9df043c613fa
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-10-16 22:21:26 +0530

    Update Oct_2025.java

--- DSA GFG/Basic/Oct_2025.java
+++ DSA GFG/Basic/Oct_2025.java
@@ -327,3 +327,21 @@ class Solution {
     }
 }
 
+// Count pair sum -> 16 Oct 2025
+
+class Solution {
+    static int countPairs(int arr1[], int arr2[], int x) {
+        // code here.
+        int count = 0;
+        
+        for(int i = 0; i < arr1.length; i++){
+            for(int j = 0; j < arr2.length; j++){
+                if(arr1[i] + arr2[j] == x){
+                    count++;
+                }
+            }
+        }     
+        return count;
+    }
+}
+
