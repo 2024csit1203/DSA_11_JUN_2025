794e01e95e04afc75ea048cce9a4fa7cc433ce42
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-10-17 23:20:40 +0530

    Update Oct_2025.java

--- LeetCode/Oct_2025.java
+++ LeetCode/Oct_2025.java
@@ -108,3 +108,23 @@ class Solution {
         return ans;
     }
 }
+
+// 283. Move Zeroes -> 17 Oct 2025
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+         int index = 0; // position to place the next non-zero element
+
+        // Move all non-zero elements to the front
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        }
+
+        // Fill the remaining positions with zeros
+        while (index < nums.length) {
+            nums[index++] = 0;
+    }
+}
+}
