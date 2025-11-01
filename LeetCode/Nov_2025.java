b20caa2ce2c37f467e060a12b21ca046f498ec64
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-11-01 23:13:06 +0530

    Implement solution for Max Consecutive Ones problem

--- LeetCode/Nov_2025.java
+++ LeetCode/Nov_2025.java
@@ -1,1 +1,22 @@
-Hi
+// 485. Max Consecutive Ones -> 01 Nov 2025
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int ans = 0;
+        int count = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+                if(count > ans){
+                    ans = count;
+                }
+            }
+            else{
+                count = 0;
+            }
+        }
+
+        return ans;
+    }
+}
