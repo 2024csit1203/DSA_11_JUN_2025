5c645496ad6c3171892b73256b1ed71a48c41259
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-11-01 21:00:59 +0530

    Update Nov_2025.java

--- DSA GFG/Basic/Nov_2025.java
+++ DSA GFG/Basic/Nov_2025.java
@@ -1,1 +1,9 @@
-Hi
+// 075. Array insert at index -> 01 Nov 2025
+
+import java.util.*;
+
+class Solution {
+    public void insertAtIndex(ArrayList<Integer> arr, int index, int val) {
+        arr.add(index, val);
+    }
+}
