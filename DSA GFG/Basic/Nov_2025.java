9ea50902c0d4962b72ae7aa55f0f4eea344629cc
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-11-09 21:22:59 +0530

    Update Nov_2025.java

--- DSA GFG/Basic/Nov_2025.java
+++ DSA GFG/Basic/Nov_2025.java
@@ -130,3 +130,28 @@ class Complete {
     }
 }
 
+// 082. Triplet Family -> 09 Nov 2025
+
+class Solution {
+    public boolean findTriplet(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int num : arr){
+            set.add(num);
+        }
+        
+        int n = arr.length;
+        for(int i = 0; i < n - 1; i++)
+        {
+            for(int j = i+1; j < n; j++){
+                int sum = arr[i] + arr[j];
+                
+                if(set.contains(sum)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
