2296041955e81d5f78a657170ffdf34bb869ff66
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-11-24 08:43:33 +0530

    Update Nov_2025.java

--- DSA GFG/Basic/Nov_2025.java
+++ DSA GFG/Basic/Nov_2025.java
@@ -875,5 +875,36 @@ class Solution {
     }
 }
 
+// 114. Face off Tournament -> 24 Nov 2025
+
+class Solution {
+    public String winner(int[] arr, int m, int n) {
+        // code here
+        int ram = 0;
+        int rohan = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % m == 0){
+                ram++;
+            }
+            
+            if(arr[i] % n == 0){
+                rohan++;
+            }
+        }
+        
+        if(ram > rohan){
+            return "Ram";
+        }
+        
+        else if(rohan > ram){
+            return "Rohan";
+        }
+        
+        return "Both";
+    }
+}
+
+
 
 
