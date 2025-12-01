86d42f07009d4a3a7a55127489b2d63c5350cad0
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-12-01 20:56:38 +0530

    Update Dec_2025.java

--- DSA GFG/Basic/Dec_2025.java
+++ DSA GFG/Basic/Dec_2025.java
@@ -1,1 +1,16 @@
-Hi
+// 126. Find sum of even factors of a number -> 01 Dec 2025
+
+class Solution {
+    static int evenFactors(int N) {
+        int sum = 0;
+        
+        for(int i = 2; i <= N; i++){
+            if(N % i == 0){
+                sum += i;
+            }
+            i++;
+        }
+        
+        return sum;
+    }
+}
