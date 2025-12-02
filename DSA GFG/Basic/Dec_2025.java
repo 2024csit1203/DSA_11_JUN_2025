effffbc1813e9cf95deb85d1469005c30e0b5496
2024csit1203 <113254189+2024csit1203@users.noreply.github.com>
2025-12-02 21:52:25 +0530

    Update Dec_2025.java

--- DSA GFG/Basic/Dec_2025.java
+++ DSA GFG/Basic/Dec_2025.java
@@ -14,3 +14,27 @@ class Solution {
         return sum;
     }
 }
+
+// 127. Parity of unsigned integer -> 02 Dec 2025
+
+class Solution {
+    static String computeParity(int N) {
+        // code here
+        String str = Integer.toBinaryString(N);
+        
+        int count = 0;
+        
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '1'){
+                count++;
+            }
+        }
+        
+        if(count % 2 == 0){
+            return "even";
+        }
+        else{
+            return "odd";
+        }
+    }
+}
